a1b87efa1e26da6add7f334b5fb02e7183237c68
hyeongyeong <kohy0329@naver.com>
2021-07-02 01:20:01 +0900

    first commit

---
+++ back/server.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const express = require('express');
+const bodyParser  = require('body-parser');
+
+// 상수
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// 앱
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+// [CONFIGURE APP TO USE bodyParser]
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
+
